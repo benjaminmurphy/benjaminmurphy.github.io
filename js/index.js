7288b1e18ed774e3f4e464bf6fedeb51d4da5164
Benjamin Murphy <benjamin_murphy@me.com>
2014-07-23 17:08:49 -0400

    Added Jekyll
    Beginning Jekyll

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  
+    $(".link_div").mouseenter(function() {
+        $(this).animate({backgroundColor: "#374140"}, 100);
+    });
+    
+    $(".link_div").mouseleave(function() {
+       $(this).animate({backgroundColor: "#D9CB9E"}, 100); 
+    });
+    
+    $("#nav_div:nth-child(1)").click(function() {
+        window.location = "
+    });
+    
+});
